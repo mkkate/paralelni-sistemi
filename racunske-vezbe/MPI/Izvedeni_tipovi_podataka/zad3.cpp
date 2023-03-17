cd272979474c2fc1ba742f4f65c2c96f4f25fa93
Katarina Mladenovic <katarina.mladenovic.kate@gmail.com>
2023-03-17 16:30:07 +0100

    MPI izvedeni tipovi zad3 i zad4

---
+++ racunske-vezbe/MPI/Izvedeni_tipovi_podataka/zad3.cpp
@@ -0,0 +1,50 @@
+﻿/*
+zad3. Napisati MPI program koji čita celobrojne (integer) i vrednosti duple tačnosti
+(double-precision) sa standardnog ulaza i saopštava to svim ostalim procesima.
+*/
+
+#define N 2
+#define MCW MPI_COMM_WORLD
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+	int rank, size;
+	struct {
+		int a;
+		double b;
+	}value;
+	int array_of_blocklengths[N];
+	MPI_Aint array_of_displacements[N];
+	MPI_Datatype array_of_types[N], newtype;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MCW, &rank);
+	MPI_Comm_size(MCW, &size);
+
+	array_of_blocklengths[0] = array_of_blocklengths[1] = 1;
+
+	MPI_Get_address(&value.a, &array_of_displacements[0]);
+	MPI_Get_address(&value.b, &array_of_displacements[1]);
+	array_of_displacements[1] -= array_of_displacements[0];
+	array_of_displacements[0] = 0;
+
+	array_of_types[0] = MPI_INT;
+	array_of_types[1] = MPI_DOUBLE;
+
+	MPI_Type_create_struct(2, array_of_blocklengths, array_of_displacements, array_of_types, &newtype);
+	MPI_Type_commit(&newtype);
+
+
+	if (rank == 0)
+		scanf_s("%d %lf", &value.a, &value.b);
+
+	MPI_Bcast(&value, 1, newtype, 0, MCW);
+
+	printf("P[%d] got %d and %lf\n", rank, value.a, value.b);
+
+	MPI_Finalize();
+
+	return 0;
+}
---
+++ racunske-vezbe/MPI/Izvedeni_tipovi_podataka/zad4.cpp
@@ -0,0 +1,101 @@
+﻿/*
+zad4. Napisati MPI program koji realizuje množenje matrice Amxn i matrice Bnxk,
+čime se dobija i prikazuje rezultujuća matrica Cmxk.
+Izračunavanje se obavlja tako što master proces šalje svakom procesu po jednu kolonu
+matrice A i po jednu vrstu matrice B.
+Svi elementi kolone matrice A se šalju odjednom.
+Svi procesi učestvuju u izračunavanjima potrebnim za generisanje rezultata programa.
+Zadatak rešiti isključivo primenom grupnih operacija.
+
+NAPOMENA: br. procesa == N !!!
+*/
+
+#define M 2
+#define N 3
+#define K 4
+#define MCW MPI_COMM_WORLD
+#include "mpi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+	int rank, size, root = 0;
+	int a[M][N]{}, b[N][K]{}, c[M][K], local_c[M][K];
+	int niz_a[M], niz_b[K];
+
+	MPI_Datatype vector, column;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MCW, &rank);
+	MPI_Comm_size(MCW, &size);
+
+	MPI_Type_vector(M, 1, N, MPI_INT, &vector);
+	MPI_Type_commit(&vector);
+	MPI_Type_create_resized(vector, 0, 1 * sizeof(int), &column);
+	MPI_Type_commit(&column);
+
+	// inicijalizacija
+	if (rank == root)
+	{
+		for (int i = 0; i < M; i++)
+			for (int j = 0; j < N; j++)
+				a[i][j] = i + j;
+
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < K; j++)
+				b[i][j] = 1 + i + j;
+
+		for (int i = 0; i < M; i++)
+			for (int j = 0; j < K; j++)
+				c[i][j] = 0;
+
+		printf("A:\n");
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < N; j++)
+				printf("%d ", a[i][j]);
+			printf("\n");
+		}
+
+		printf("B:\n");
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < K; j++)
+				printf("%d ", b[i][j]);
+			printf("\n");
+		}
+
+		printf("C:\n");
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < K; j++)
+				printf("%d ", c[i][j]);
+			printf("\n");
+		}
+	}
+
+	MPI_Scatter(a, 1, column, niz_a, M, MPI_INT, root, MCW);
+	MPI_Scatter(b, K, MPI_INT, niz_b, K, MPI_INT, root, MCW);
+
+	for (int i = 0; i < M; i++)
+		for (int j = 0; j < K; j++)
+			local_c[i][j] = niz_a[i] * niz_b[j];
+
+	MPI_Reduce(local_c, c, M * K, MPI_INT, MPI_SUM, root, MCW);
+
+	if (rank == root)
+	{
+		printf("Rezultujuca C:\n");
+		for (int i = 0; i < M; i++)
+		{
+			for (int j = 0; j < K; j++)
+				printf("%d ", c[i][j]);
+			printf("\n");
+		}
+	}
+
+	MPI_Finalize();
+
+	return 0;
+}
